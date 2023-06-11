5998384118b14142b060d4ca6e2cc15e2b792048
Xu Xiang <118589649+MuaCherish@users.noreply.github.com>
2023-06-11 12:18:24 +0800

    Update 刷课模板.py

--- 通过index刷课模板.py
+++ 通过index刷课模板.py
@@ -1,9 +1,15 @@
 import pyautogui
 import time
 
+# 自定义
+Display_x = (628, 801)              # 电脑播放键x范围
+Display_y = (849, 1046)             # 电脑播放键y范围
+index = [(933, 1737), (769, 937)]   # 鼠标坐标记录
+
 # 初始化
-index = []
 i = 0
+x_min, x_max = Display_x
+y_min, y_max = Display_y
 
 # 开始刷课
 while i < len(index):
@@ -12,7 +18,7 @@ while i < len(index):
 
     # 判断是不是播放键，是的话就开启长时间等待,不是就等待3秒钟
     x, y = index[i]
-    if 628 <= x <= 801 and 849 <= y <= 1046:  # 是播放键
+    if x_min <= x <= x_max and y_min <= y <= y_max:  # 是播放键
         for j in range(600):  # 等待视频刷完
             print(f'视频已经播放了{j}秒')
             time.sleep(1)
